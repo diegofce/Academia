36906ce3d89f04e63613145a01f8d783994b87df
DiegoFChacon <chacondiego31@gmail.com>
2025-06-25 19:34:57 -0500

    Actualizar el menú principal para utilizar la opción del docente en lugar de la opción estándar

--- proyecto_mod_5/main.py
+++ proyecto_mod_5/main.py
@@ -1,11 +1,13 @@
 from views.menu import menu_principal
+from views.menu_docente import menu_principal_docente
 from config.database import Database
 
 if __name__ == "__main__":
     db = Database()
     try:
         # Iniciar el menú principal
-        menu_principal(db)
+        # menu_principal(db)
+        menu_principal_docente(db)
     finally:
         # Asegurarse de cerrar la conexión a la base de datos al finalizar
         db.close()
