6d874763cff4f5cccfa7c4827654ab9b885b54e1
DiegoFChacon <chacondiego31@gmail.com>
2025-06-25 20:55:54 -0500

    Agregar menú principal para gestionar cursos y comentar opción de menú docente en el archivo main.py

--- proyecto_mod_5/main.py
+++ proyecto_mod_5/main.py
@@ -1,13 +1,16 @@
 from views.menu import menu_principal
 from views.menu_docente import menu_principal_docente
+from views.menu_curso import menu_principal_curso
 from config.database import Database
+
 
 if __name__ == "__main__":
     db = Database()
     try:
         # Iniciar el menú principal
         # menu_principal(db) #Menu de estudiantes
-        menu_principal_docente(db)
+        # menu_principal_docente(db) #Menu de docentes
+        menu_principal_curso(db) #Menu de cursos
     finally:
         # Asegurarse de cerrar la conexión a la base de datos al finalizar
         db.close()
